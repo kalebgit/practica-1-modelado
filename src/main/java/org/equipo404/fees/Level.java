ef35761a71b6a3c727b91397e1d2c8f62bbc3c6c
Saul-ss0 <sansotsau004@gmail.com>
2025-02-22 19:12:11 -0600

    Update Level.java

--- src/main/java/org/equipo404/fees/Level.java
+++ src/main/java/org/equipo404/fees/Level.java
@@ -1,5 +1,21 @@
 package org.equipo404.fees;
-
+/**
+ * Enum que representa los niveles disponibles para los cursos.
+ * Los niveles definen el grado de complejidad del curso y pueden ser utilizados para determinar
+ * el precio y el contenido del curso.
+ * 
+ * Los niveles definidos son:
+ * - **BÁSICO**: Nivel inicial del curso.
+ * - **INTERMEDIO**: Nivel intermedio del curso.
+ * - **AVANZADO**: Nivel avanzado del curso.
+ * 
+ * @author Emiliano Kaleb Jimenez Rivera, Bedoya Tellez Ariadna Valeria, Sanchez Soto Saul
+ * @version 1.0
+ */
 public enum Level {
+    /**
+     * Nivel básico de los cursos.
+     * Representa un nivel inicial con contenidos fundamentales.
+     */
     BASICO, INTERMEDIO, AVANZADO
 }
