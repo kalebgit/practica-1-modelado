3730e59137534284baf11edddddbeaa5693be90a
Saul-ss0 <sansotsau004@gmail.com>
2025-02-22 19:16:12 -0600

    Update PracticalLearning.java

--- src/main/java/org/equipo404/learningmodes/PracticalLearning.java
+++ src/main/java/org/equipo404/learningmodes/PracticalLearning.java
@@ -1,6 +1,17 @@
 package org.equipo404.learningmodes;
+/**
+ * Implementación concreta de la interfaz {@link LearningModeStrategy} que representa la modalidad de aprendizaje práctico.
+ * Esta modalidad se centra en la aplicación de conceptos a través de actividades prácticas y ejercicios.
+ * 
+ * @author Emiliano Kaleb Jimenez Rivera, Bedoya Tellez Ariadna Valeria, Sanchez Soto Saul
+ * @version 1.0
+ */
 
 public class PracticalLearning implements LearningModeStrategy{
+    /**
+     * Implementación del método {@link LearningModeStrategy#learn()} para la modalidad de aprendizaje práctico.
+     * Este método simula el aprendizaje a través de actividades prácticas, mostrando un mensaje en consola.
+     */
     @Override
     public void learn() {
         System.out.println("aprendiendo práctica");
