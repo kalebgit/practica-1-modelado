3331adc1f390402e978fd4b81668bc2802d604b1
Saul-ss0 <sansotsau004@gmail.com>
2025-02-22 19:11:06 -0600

    Update CoursesPrice.java

--- src/main/java/org/equipo404/fees/CoursesPrice.java
+++ src/main/java/org/equipo404/fees/CoursesPrice.java
@@ -3,8 +3,19 @@ package org.equipo404.fees;
 import org.equipo404.courses.CourseType;
 
 import java.util.Map;
+/**
+ * Clase que gestiona los precios de los cursos según su tipo y nivel.
+ * Utiliza un mapa estático para almacenar los precios de los cursos, organizados por tipo de curso y nivel.
+ * 
+ * @author Emiliano Kaleb Jimenez Rivera, Bedoya Tellez Ariadna Valeria, Sanchez Soto Saul
+ * @version 1.0
+ */
 
 public  class CoursesPrice {
+    /**
+     * Mapa estático que contiene los precios de los cursos organizados por tipo de curso y nivel.
+     * La estructura es un mapa de tipo `CourseType` a un mapa de `Level` a `Double` (precio del curso).
+     */
 
     private static Map<CourseType, Map<Level, Double>> PRICES = Map.of(
             CourseType.PROGRAMACION_JAVA, Map.of(
@@ -23,6 +34,13 @@ public  class CoursesPrice {
                     Level.AVANZADO, 150.0
             )
     );
+    /**
+     * Obtiene el precio de un curso específico según su tipo y nivel.
+     * 
+     * @param course Tipo de curso para el cual se desea obtener el precio.
+     * @param level Nivel del curso (por ejemplo, básico, intermedio, avanzado).
+     * @return El precio del curso correspondiente. Si el curso o nivel no están definidos, retorna 0.0.
+     */
 
     public static double getPrice(CourseType course, Level level) {
          Double result = PRICES.getOrDefault(course, Map.of()).getOrDefault(level, 0.0);
