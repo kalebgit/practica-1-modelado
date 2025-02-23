7325fc55fa77f9570d89b952665583cb067b3721
Saul-ss0 <sansotsau004@gmail.com>
2025-02-22 19:18:18 -0600

    Update VisualLearning.java

--- src/main/java/org/equipo404/learningmodes/VisualLearning.java
+++ src/main/java/org/equipo404/learningmodes/VisualLearning.java
@@ -1,6 +1,16 @@
 package org.equipo404.learningmodes;
-
+/**
+ * Implementación concreta de la interfaz {@link LearningModeStrategy} que representa la modalidad de aprendizaje visual.
+ * Esta modalidad se enfoca en la utilización de recursos visuales, como videos, diagramas, y gráficos, para facilitar el aprendizaje.
+ * 
+ * @author Emiliano Kaleb Jimenez Rivera, Bedoya Tellez Ariadna Valeria, Sanchez Soto Saul
+ * @version 1.0
+ */
 public class VisualLearning implements LearningModeStrategy{
+    /**
+     * Implementación del método {@link LearningModeStrategy#learn()} para la modalidad de aprendizaje visual.
+     * Este método simula el aprendizaje utilizando recursos visuales, mostrando un mensaje en consola.
+     */
 
     @Override
     public void learn() {
